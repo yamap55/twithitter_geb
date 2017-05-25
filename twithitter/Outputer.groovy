b6ed0964a4325f96d84a879b244d58efb27f5ed2
yamap55 <tetuya120@hotmail.com>
2017-05-25 23:34:23 +0900

    [add] comment

--- twithitter/Outputer.groovy
+++ twithitter/Outputer.groovy
@@ -5,8 +5,11 @@ import static groovyx.net.http.ContentType.*
 
 class Outputer {
   def outputF
+
+  // 出力クラス
   Outputer(googleWebAplicationId, batterResultFile, pitcherResultFile) {
     if (googleWebAplicationId) {
+      // GoogleスプレッドシートにアクセスするIDが有効な場合はスプレッドシートに書き込む。
       outputF = {user ->
         def http = new HTTPBuilder( "https://script.google.com/macros/s/${googleWebAplicationId}/" )
         http.post( path: 'exec', body: user.asJson(),
@@ -15,6 +18,7 @@ class Outputer {
         }
       }
     } else {
+      // GoogleスプレッドシートにアクセスするIDが無効な場合はファイルに出力する。
       outputF = { user ->
         def result = user.isBatter() ? batterResultFile : pitcherResultFile
         result << "${user.asCsvString()}\n"
